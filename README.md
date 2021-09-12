cb1d260304a70ec09235bc792282eba617bf14a4
Jorgen <jorgen@buildnode.io>
2021-09-12 11:58:55 -0700

    deploy to mainnet

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tourney: Metascore's Frontend Application
 
+https://oagmd-5iaaa-aaaah-qbnma-cai.raw.ic0.app/
+
 ![App Mockup](mockup.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,28 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import { createActor } from '@metascore/query-staging';
+import { createActor, Metadata } from '@metascore/query-staging';
 
 function App() {
 
-  const [count, setCount] = useState(0);
   const metascoreQuery = useMemo(() => createActor(), []);
+  
+  const [count, setCount] = useState(0);
+  const [games, setGames] = useState<Metadata[]>();
 
-  metascoreQuery.getGames().then(console.log);
+  useEffect(() => {
+    metascoreQuery.getGames().then(setGames);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <h1>Games</h1>
+        <ul>
+          {games?.map(game => <li>{game.name}</li>)}
+        </ul>
+        <h1></h1>
       </header>
     </div>
   )
