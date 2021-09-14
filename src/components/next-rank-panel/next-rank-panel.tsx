d7f518f8df81c372c4f2351d028f60df853840bc
Jorgen <jorgen@buildnode.io>
2021-09-14 14:01:06 -0700

    next rank panel layout

--- src/components/next-rank-panel/next-rank-panel.tsx
+++ src/components/next-rank-panel/next-rank-panel.tsx
@@ -2,6 +2,12 @@ import Panel, { Label } from 'components/panel/panel';
 import Progress from 'components/progress/progress';
 import React from 'react';
 import Styles from './next-rank-panel.module.css';
+import Badge1 from 'assets/badges/1.webp';
+import Badge2 from 'assets/badges/2.webp';
+import Badge3 from 'assets/badges/3.webp';
+import Badge4 from 'assets/badges/4.webp';
+import Badge5 from 'assets/badges/5.webp';
+import Badge6 from 'assets/badges/6.webp';
 
 interface Props {
     children?: React.ReactNode;
@@ -14,6 +20,10 @@ export default function NextRankPanel ({ children } : Props) {
                 <Label>Next Rank</Label>
                 <h4>Gamer</h4>
                 <Progress />
+                <div className={Styles.badgeContainer}>
+                    <img className={Styles.badge} width={56} height={56} src={Badge1} />
+                    <div className={Styles.text}>~1 Trillion Points</div>
+                </div>
             </Panel>
         </div>
     );
