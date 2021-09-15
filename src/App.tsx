cfbf0e1c5975cfabdd51cbc219bce43f2eddf4e4
jorgenbuilder <jorgen@buildnode.io>
2021-09-15 09:39:51 -0700

    add WIP panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { AnimatedRoute, AnimatedSwitch } from 'components/animated-route';
 import Container from 'components/container/container';
 import Grid, { GridRow } from 'components/grid/grid';
 import Head from 'components/head/head';
+import Panel from 'components/panel/panel';
 import GamesProvider from 'context/games';
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Connect from 'screens/connect';
 import Guide from 'screens/guide';
 import Plug from 'screens/plug';
@@ -25,6 +26,13 @@ export default function App() {
                     <Grid>
                         <GridRow>
                             <Head />
+                        </GridRow>
+                        <GridRow>
+                            <Panel>
+                                <div>
+                                    <strong>🚨🚨 Heads Up!</strong> The app is builing built 🔨🔨 <em>right now!</em> You can <Link className='hyperlink' to='/guide'>read the guide</Link> 📖🧠 and feel free to click around, but most things don't work yet!
+                                </div>
+                            </Panel>
                         </GridRow>
                     </Grid>
                     <AnimatedSwitch>
