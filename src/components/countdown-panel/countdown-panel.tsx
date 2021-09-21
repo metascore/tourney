cfc1a166010ba4d9ae91e04fcbe42bc5702c208d
Jorgen <jorgen@buildnode.io>
2021-09-20 22:13:32 -0700

    countdown tournament states

--- src/components/countdown-panel/countdown-panel.tsx
+++ src/components/countdown-panel/countdown-panel.tsx
@@ -2,31 +2,48 @@ import React from 'react';
 import Styles from './countdown-panel.module.css';
 import Panel, { Label } from 'components/panel/panel';
 
+const start = new Date ('September 22 2021 00:00:00').getTime();
+const end = new Date ('September 26 2021 00:00:00').getTime();
+
 interface Props {
     children?: React.ReactNode;
 };
 
 export default function CountdownPanel ({ children } : Props) {
-    const stopCount = new Date ('September 22 2021 00:00:00');
-    const [countdown, setCountdown] = React.useState<number>(new Date(stopCount).getTime() - new Date().getTime());
+    const now = new Date().getTime();
+
+    const status : 'countdown' | 'on' | 'done' = 
+        now <= start
+        ? 'countdown'
+        : now < end
+            ? 'on'
+            : 'done';
+    
+    const zero : number = status === 'countdown' ? start : end;
+
+    const [delta, setDelta] = React.useState<number>(zero - now);
 
     React.useEffect(() => {
-        const iter = setInterval(() => {
-            setCountdown(new Date(stopCount).getTime() - new Date().getTime());
-        }, 1000)
-        return () => clearInterval(iter)
-    }, []);
+        if (status !== 'done') {
+            console.log(status);
+            const iter = setInterval(() => {
+                setDelta(Math.max(zero - new Date().getTime(), 0));
+            }, 1000)
+            return () => clearInterval(iter)
+        }
+    }, [status, now]);
+
     return (
         <div className={Styles.root}>
             <Panel>
                 <Label>
                     <div className={Styles.label}>
-                        <div>Tournament Begins</div>
-                        <div>Midnight, Sept 22</div>
+                        <div>Tournament {status === 'countdown' ? 'Begins' : status === 'on' ? 'Ends' : 'Finished'}</div>
+                        <div>Midnight, Sept {new Date(zero).getDate()}</div>
                     </div>
                 </Label>
                 <div className={Styles.countdown}>
-                    {countdownFormat(countdown)}
+                    {countdownFormat(delta)}
                 </div>
             </Panel>
         </div>
@@ -38,6 +55,7 @@ function pad(n: number) { return n < 10 ? `0${n}` : `${n}` }
 export function countdownFormat(countdown: number) : string {
     // This expects a number representing a duration of time remaining.
     // Returns a string "0:00:00:00"
+    console.log(countdown)
     const t = countdown / 1000;
     const d = Math.floor(t / (60 * 60 * 24));
     const h = Math.floor(t % (60 * 60 * 24) / (60 * 60));
